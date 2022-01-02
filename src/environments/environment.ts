c085b372086d936a475e09900b17fd0c7bb77dcb
Hardikkumar <38484556+katakiya@users.noreply.github.com>
2022-01-02 18:38:14 +0530

    Removed my firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBMZo88W3C0J9SQ1VbaeXVYTdEwrvr8HE0",
-    authDomain: "e-learning-platform-56d38.firebaseapp.com",
-    projectId: "e-learning-platform-56d38",
-    storageBucket: "e-learning-platform-56d38.appspot.com",
-    messagingSenderId: "769752122790",
-    appId: "1:769752122790:web:b1de84fc03461509e0fb67"
+    // Add Your firebase config here
   }
 };
 
