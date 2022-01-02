c2bdfbec7eecca5a63d3882f95c699e2767839a1
Hardikkumar <38484556+katakiya@users.noreply.github.com>
2022-01-02 18:39:14 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBMZo88W3C0J9SQ1VbaeXVYTdEwrvr8HE0",
-    authDomain: "e-learning-platform-56d38.firebaseapp.com",
-    projectId: "e-learning-platform-56d38",
-    storageBucket: "e-learning-platform-56d38.appspot.com",
-    messagingSenderId: "769752122790",
-    appId: "1:769752122790:web:b1de84fc03461509e0fb67"
+     // add your firebase config here.
   }
 };
